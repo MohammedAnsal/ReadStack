c9b32199de0b461d8bfdec987bc96063405f754b
MohammedAnsal <ansalshaah786@gmail.com>
2025-12-24 14:35:50 +0530

    add: add snowfall component

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,9 +4,11 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routers/UserRouters";
 import { Toaster } from "react-hot-toast";
+import Snowfall from 'react-snowfall'
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Snowfall color="#82C3D9"/>
     <Toaster position="top-right" reverseOrder={false} />
     <RouterProvider router={router} />
   </StrictMode>
